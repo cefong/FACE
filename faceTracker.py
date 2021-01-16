61194b2a33f13c80bca388c90e74f39d72418201
Toufic <toufics99@gmail.com>
2021-01-16 15:31:13 -0700

    facial landmark detection

--- faceTracker.py
+++ faceTracker.py
@@ -5,12 +5,14 @@ import dlib
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
+predictor  = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
+    
     
     for face in faces:
         x1 = face.left()
@@ -19,6 +21,13 @@ while True:
         y2 = face.bottom()
 
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 255, 0), 3)
+
+        landmarks = predictor(gray, face)
+
+    for i in range(0, 68):
+        x = landmarks.part(i).x
+        y = landmarks.part(i).y
+        cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
 
     cv2.imshow("Frame", frame)
 

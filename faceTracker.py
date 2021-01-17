332a2121291fa2e35f5cd03624eff04550d2dd89
Toufic <toufics99@gmail.com>
2021-01-16 17:25:59 -0700

    blinking detection

--- faceTracker.py
+++ faceTracker.py
@@ -1,8 +1,30 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
 from pynput.mouse import Button, Controller
 from util import measureTiltDistance
+font = cv2.FONT_HERSHEY_COMPLEX
+blink_ratio = 0.21 
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def get_eye_ratio(eye_points, facial_landmarks):
+    
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - left_point[1]))
+    ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+    ratio = ver_line_length/hor_line_length
+
+    return ratio
+
+
 
 cap = cv2.VideoCapture(0)
 
@@ -44,11 +66,27 @@ while True:
 
         landmarks = predictor(gray, face)
 
+        # numbers 36 - 41 depict the points associated with the left eye
+        left_ratio = get_eye_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        
+        # numbers 42 - 47 depict right eye points
+        right_ratio = get_eye_ratio([42,43, 44, 45, 46, 47], landmarks)
+        ratio_average = (right_ratio + left_ratio)/2
+
+        
+
+        if left_ratio < blink_ratio and right_ratio > blink_ratio:
+            cv2.putText(frame, "Left Blink.", (100,100), font, 2, (0, 0, 255), 5)
+        if right_ratio < blink_ratio and left_ratio > blink_ratio:
+            cv2.putText(frame, "Right Blink.", (200,200), font, 2, (255, 0, 0), 5)
+        if left_ratio < blink_ratio and right_ratio < blink_ratio:
+            cv2.putText(frame, "Blinking.", (150,350), font, 2, (0, 255, 0), 5)
+    '''
         for i in range(0, 68):
             x = landmarks.part(i).x
             y = landmarks.part(i).y
             cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-
+    '''
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)

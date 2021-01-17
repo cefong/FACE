1965c830b2fcff2b5ecc97df1a9608112d9e352f
Celine Fong <celinefong4@gmail.com>
2021-01-17 02:15:59 -0700

    documented and final fixes

--- faceTracker.py
+++ faceTracker.py
@@ -1,11 +1,12 @@
 import cv2
-import numpy as np
 import dlib
 
 from util import *
 
+# sets the video capture to the built in webcam
 cap = cv2.VideoCapture(0)
 
+# gets the face detector from dlib
 detector = dlib.get_frontal_face_detector()
 predictor  = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
@@ -19,32 +20,46 @@ mouse = Controller()
 # set scale of stillbox
 scale = 8
 
-# set the max size of the buffer
+# set the starting point of the frequency calculator
 side = 0
 blink = 0
 mouth = 0
 
+# infinite loop keeps generating new frames
 while True:
+
+    # gets the current frame from the video capture
     _, frame = cap.read()
+
+    # flip the fram so it mirrors the user
     frame = cv2.flip(frame, 1)
 
+    # grayscale the image (since face detector detects better in grayscale)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # detect face
     faces = detector(gray)
 
+    # only let one face control the computer interface
     if len(faces) > 0:
         face = faces[0]
+
+        # get the boundaries of the face detected
         x1 = face.left()
         y1 = face.top()
         x2 = face.right()
         y2 = face.bottom()
 
+        # the current position of the face provides these parameters
         currentPosition = [x1, y1, x2, y2]
 
+        # draw a rectangle around the face that is being used for controls
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0, 255, 0), 3)
 
+        # calculate 68 facial landmarks
         landmarks = predictor(gray, face)
 
+        # draw a dot on the landmark being tracked for the double-click box
         x = landmarks.part(36).x
         y = landmarks.part(36).y
         cv2.circle(frame, (x, y), 3, (0, 0, 255), -1)
@@ -62,8 +77,9 @@ while True:
        #if mouth_ratio > 0.7:
         #        cv2.putText(frame, "MOUTH OPEN.", (300, 300), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0))
         mouthInc = detect_mouth_open(mouth_ratio)
-        mouth += mouthInc
+        mouth = max(0, mouthInc + mouth)
 
+        # if there have been over 3 mouth triggers
         if mouth > 3:
             r = sr.Recognizer()
             keyboard = keyboardController()
@@ -82,53 +98,61 @@ while True:
                 print("Sorry, I did not get that")
             mouth = 0
 
+        # calculate the amount to increment blink and side triggers
         blinkInc, sideInc = detectBlink(left_ratio, right_ratio)
-        blink += blinkInc
+        blink = max(0, blinkInc + blink)
 
+        # only do facial tilt calculations after calibration of initial postiion has been done
         if (initialPosition != [0, 0, 0, 0]):
             cv2.rectangle(frame, (initialPosition[0] - scale, initialPosition[1] + scale), (initialPosition[2] - scale, initialPosition[3] + scale), (255, 0, 0), 3)
+
             # draw rectangle for double click area
             quarter = 3*int(initialLength/4)
             cv2.rectangle(frame, (initialPosition[0] - scale, initialPosition[1] + scale), (initialPosition[2] - quarter, initialPosition[3] + scale), (255, 255, 0), 3)
 
+            # move the mouse according to the relative distance of the current position of the head to the calibrated
+            # initial position
             difference = measureTiltDistance(initialPosition[:2], currentPosition[:2], scale)
             mouse.move(difference[0], difference[1])
             side += sideInc
-            # print("side value: " + str(side))
+
+            # if the side is more negative it indicates a likely left wink (which maps to a left click)
             if side < -2:
-                print("Current position of point: " + str(landmarks.part(36).x))
-                print("Lower Bound: " + str(initialPosition[0] - scale))
-                print("Upper Bound: " + str(initialPosition[2] - quarter))
+                # if the left eye landmark is in the double click box, register a double click when they wink
                 if (landmarks.part(36).x > initialPosition[0] - scale and landmarks.part(36).x < initialPosition[2] - quarter):
                     mouse.click(Button.left, 2)
                     cv2.putText(frame, "Double left click.", (100,100), font, 2, (0, 0, 255), 5)
+                # register a normal single left click if they left eye landmark is not in the double click box
                 else:
                     mouse.press(Button.left)
                     mouse.release(Button.left)
                     cv2.putText(frame, "Left click.", (100,100), font, 2, (0, 0, 255), 5)
                 side = 0
+            # if the side is more positive it indicates a likely right wink (maps to a right click)
             if side > 7:
                 mouse.press(Button.right)
                 mouse.release(Button.right)
                 side = 0
                 cv2.putText(frame, "Right Blink.", (200,200), font, 2, (255, 0, 0), 5)
-        if blink > 20:
+        # if the blink has been triggered over 25 times, register a new calibrated initial position to measure facial tilt from
+        if blink > 25:
             blink = 0
             cv2.putText(frame, "Blinking.", (150,350), font, 2, (0, 255, 0), 5)
             initialPosition = [x1, y1, x2, y2]
             initialLength = x2 - x1
-    '''
-        for i in range(0, 68):
-            x = landmarks.part(i).x
-            y = landmarks.part(i).y
-            cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-    '''
+
+    # show the frame on a window
     cv2.imshow("Frame", frame)
 
+    # register a key
     key = cv2.waitKey(1)
+
+    # if the spacebar is hit, trigger a new calibarated intial position
     if key == 32:
         initialPosition = [x1, y1, x2, y2]
         initialLength = x2 - x1
+
+    # if the esc key is hit, trigger the exit of the program
     if key == 27:
         break
 
--- util.py
+++ util.py
@@ -1,21 +1,21 @@
 import cv2
+import speech_recognition as sr
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Controller as keyboardController
-import speech_recognition as sr
-
 from math import hypot
 
 # measure how far the head tilt is from the initial position
 def measureTiltDistance(initialPosition, currentPosition, scale):
     difference = [0,0]
+
     # tilt left
     difference[0] = (currentPosition[0] - initialPosition[0])/10
     if (difference[0] > -scale and difference[0] < scale):
         difference[0] = 0
 
     # tilt top
-    difference[1] = (currentPosition[1] - initialPosition[1])/7
-    if (difference[0] > -(scale+2) and difference[0] < (scale-2)):
+    difference[1] = (currentPosition[1] - initialPosition[1])/10
+    if (difference[0] > -(scale + 2) and difference[0] < (scale - 2)):
         difference[0] = 0
     return difference
 
@@ -23,9 +23,11 @@ def measureTiltDistance(initialPosition, currentPosition, scale):
 font = cv2.FONT_HERSHEY_COMPLEX
 blink_ratio = 0.21
 
+# get the midpoint of the eye
 def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
+# get the ratio of the eye
 def get_eye_ratio(eye_points, facial_landmarks):
 
     left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
@@ -40,6 +42,7 @@ def get_eye_ratio(eye_points, facial_landmarks):
 
     return ratio
 
+# detects what type of activity is happening in the eyes - left wink, right wink, full blink, or none
 def detectBlink(left_ratio, right_ratio):
     if left_ratio < blink_ratio and right_ratio < blink_ratio:
         return (1, 0)
@@ -51,14 +54,14 @@ def detectBlink(left_ratio, right_ratio):
         return (0, 1)
     else:
         # blink--
-        return (0, 0)
+        return (-0.5, 0)
 
 # function to detect mouth ratio
 def get_mouth_ratio(mouth_points, facial_landmarks):
 
     center_top = (facial_landmarks.part(mouth_points[2]).x, facial_landmarks.part(mouth_points[2]).y)
     center_bottom = (facial_landmarks.part(mouth_points[6]).x, facial_landmarks.part(mouth_points[6]).y)
-    left_point = (facial_landmarks.part(mouth_points[0]).x, facial_landmarks.part(mouth_points[0]).y) 
+    left_point = (facial_landmarks.part(mouth_points[0]).x, facial_landmarks.part(mouth_points[0]).y)
     right_point = (facial_landmarks.part(mouth_points[4]).x, facial_landmarks.part(mouth_points[4]).y)
 
     hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
@@ -67,9 +70,10 @@ def get_mouth_ratio(mouth_points, facial_landmarks):
     ratio = ver_line_length/hor_line_length
     return ratio
 
+# function to detect if mouth is open according to mouth ratio calculated
 def detect_mouth_open(mouth_ratio):
 
     if mouth_ratio > 0.8:
         return 1
 
-    return 0
+    return -0.5
